56493cde0de0bddaf6833bf2047ce0c88d135ab3
Marie Vabamägi <marievabamagi@Maries-Air.lan>
2024-04-12 15:57:21 +0300

    clean code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { NameFormComponent } from './name-form/name-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NameFormComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     ReactiveFormsModule
